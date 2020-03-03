a422f5b8739e632774336e04c2a5c2023975c2d2
caijingzhuang <caijingzhuang@gmail.com>
2020-03-03 21:59:51 +0800

    init lib

---
+++ Example/Pods/Target Support Files/Pods-SSKit_Tests/Pods-SSKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SSKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SSKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/SSKit/SSKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SSKit : NSObject
+@end
+@implementation PodsDummy_SSKit
+@end
